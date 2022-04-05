eaffbcbbff79fa86d308b8f3e376df4335dfd8cf
Robert <robertjwisniowski@gmail.com>
2022-04-05 22:44:27 +0200

    Raycasting for Components added. Action priority added.

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attck(CombatTarget target)
+        {
+            print("Feel the force!");
+        }    
+    }
+}
+
+
