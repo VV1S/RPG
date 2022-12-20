51c7adac59646fca961009ce84d298289340292b
Robert <robertjwisniowski@gmail.com>
2022-12-20 23:51:24 +0100

    Experience can be saved now.

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,14 +1,25 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Saving;
+using UnityEngine;
 
 namespace RPG.Attributes
 {
-    internal class Experience: MonoBehaviour
+    internal class Experience: MonoBehaviour, ISaveable
     {
         [SerializeField] private float experiencePoints = 0;
 
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
         }
     }
 }
